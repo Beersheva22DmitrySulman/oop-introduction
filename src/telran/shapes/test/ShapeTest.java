b534d5da2dbcec111829bc42b7b4c8b15b9f7209
Dmitry Sulman <dmitry.sulman@gmail.com>
2022-12-01 15:49:16 +0200

    CW 12

--- src/telran/shapes/test/ShapeTest.java
+++ src/telran/shapes/test/ShapeTest.java
@@ -11,10 +11,14 @@ class ShapeTest {
 	@Test
 	void rectangleTest() {
 		Rectangle rectangle = new Rectangle(50, 10);
-		drawLines(rectangle.presentation(15));
+		assertEquals(50, rectangle.getWidth());
+		assertEquals(10, rectangle.getHeight());
+		displayStrings(rectangle.presentation(15));
+		Rectangle.setSymbol("#");
+		displayStrings(rectangle.presentation(15));
 	}
 	
-	private void drawLines(String[] lines) {
+	private void displayStrings(String[] lines) {
 		for (String line : lines) {
 			System.out.println(line);
 		}
