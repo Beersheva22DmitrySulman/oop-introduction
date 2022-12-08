71ffbdab505e7d3aec9c0066531026661baa1b98
Dmitry Sulman <dmitry.sulman@gmail.com>
2022-12-08 17:38:20 +0200

    HW 14

--- src/telran/cipher/BaseCipher.java
+++ src/telran/cipher/BaseCipher.java
@@ -19,11 +19,11 @@ public class BaseCipher {
 	
 	public String cipher(int number) {
 		int numberLength = getNumberLength(number);
-		int keyLength = key.length();
+		int base = key.length();
 		char[] charArray = new char[numberLength];
 		for (int i = numberLength - 1; i >=0; i--) {
-			charArray[i] = key.charAt(number % keyLength);
-			number /= keyLength;
+			charArray[i] = key.charAt(number % base);
+			number /= base;
 		}
 		
 		return String.valueOf(charArray);
@@ -31,10 +31,10 @@ public class BaseCipher {
 	
 	public int decipher(String cipher) {
 		int cipherLength = cipher.length();
-		int keyLength = key.length();
+		int base = key.length();
 		int res = 0;
 		for (int i = 0; i < cipherLength; i++) {
-			res = res * keyLength + key.indexOf(cipher.charAt(i));
+			res = res * base + key.indexOf(cipher.charAt(i));
 		}
 		
 		return res;
@@ -67,10 +67,10 @@ public class BaseCipher {
 	}
 	
 	private int getNumberLength(int number) {
-		int keyLength = key.length();
+		int base = key.length();
 		int res = 0;
 		do {
-			number /= keyLength;
+			number /= base;
 			res++;
 		} while (number != 0);
 		
