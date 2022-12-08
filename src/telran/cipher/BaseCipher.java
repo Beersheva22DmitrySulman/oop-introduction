b6f24ff0bfc4b968210c107f6ad5d55a107da311
Dmitry Sulman <dmitry.sulman@gmail.com>
2022-12-08 18:59:18 +0200

    HW 14

--- src/telran/cipher/BaseCipher.java
+++ src/telran/cipher/BaseCipher.java
@@ -4,31 +4,31 @@ import java.util.Random;
 
 public class BaseCipher {
 	private String key;
-	
+
 	private static final int MIN_CODE = 33;
 	private static final int MAX_CODE = 126;
-	
+
 	private static final int MIN_LENGTH = 2;
 	private static final int MAX_LENGTH = MAX_CODE - MIN_CODE + 1;
-	
+
 	public BaseCipher(int length) {
 		length = Math.max(length, MIN_LENGTH);
 		length = Math.min(length, MAX_LENGTH);
 		key = generateKey(length);
 	}
-	
+
 	public String cipher(int number) {
 		int numberLength = getNumberLength(number);
 		int base = key.length();
 		char[] charArray = new char[numberLength];
-		for (int i = numberLength - 1; i >=0; i--) {
+		for (int i = numberLength - 1; i >= 0; i--) {
 			charArray[i] = key.charAt(number % base);
 			number /= base;
 		}
-		
+
 		return String.valueOf(charArray);
 	}
-	
+
 	public int decipher(String cipher) {
 		int cipherLength = cipher.length();
 		int base = key.length();
@@ -40,14 +40,14 @@ public class BaseCipher {
 			}
 			res = res * base + digit;
 		}
-		
+
 		return res;
 	}
 
 	public String getKey() {
 		return key;
 	}
-	
+
 	private String generateKey(int length) {
 		char[] charArray = new char[length];
 		boolean[] helper = new boolean[MAX_CODE - MIN_CODE + 1];
@@ -56,20 +56,20 @@ public class BaseCipher {
 			helper[num - MIN_CODE] = true;
 			charArray[i] = (char) num;
 		}
-		
+
 		return String.valueOf(charArray);
 	}
-	
+
 	private int generateUniqueRandomNumber(boolean[] helper) {
 		Random random = new Random();
 		int num;
 		do {
 			num = random.nextInt(MIN_CODE, MAX_CODE + 1);
 		} while (helper[num - MIN_CODE]);
-		
+
 		return num;
 	}
-	
+
 	private int getNumberLength(int number) {
 		int base = key.length();
 		int res = 0;
@@ -77,7 +77,7 @@ public class BaseCipher {
 			number /= base;
 			res++;
 		} while (number != 0);
-		
+
 		return res;
 	}
 }
