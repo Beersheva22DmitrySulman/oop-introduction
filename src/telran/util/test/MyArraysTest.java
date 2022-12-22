a5ca43aa1a26c2b44b1842f360bb8de3fc402584
Dmitry Sulman <dmitry.sulman@gmail.com>
2022-12-22 18:32:19 +0200

    HW 17

--- src/telran/util/test/MyArraysTest.java
+++ src/telran/util/test/MyArraysTest.java
@@ -61,38 +61,41 @@ class MyArraysTest {
 
 		Integer[] expectedNumbers = { 2, -8, 100, 10 };
 		String[] expectedStr = { "abm", "abmb", "abmbc" };
-		
+
 		assertArrayEquals(expectedNumbers, MyArrays.filter(numbers, predEven));
 		assertArrayEquals(expectedStr, MyArrays.filter(strings, predSubstr));
 	}
-	
+
 	@Test
 	void removeIfTest() {
 		Integer[] expectedNumbers = { 13, 47, -7, 7 };
 		String[] expectedStr = { "ab" };
-		
+
 		assertArrayEquals(expectedNumbers, MyArrays.removeIf(numbers, x -> x % 2 == 0));
 		assertArrayEquals(expectedStr, MyArrays.removeIf(strings, x -> x.contains("m")));
 	}
-	
+
 	@Test
 	void removeRepeatedTest() {
-		Integer[] nums1 = { 0, 1, 1, 2, 2, 1, 1, 3, 3, -10, 15, 1, 22, 23, 15, 7, 8, 0};
+		Integer[] nums1 = { 0, 1, 1, 2, 2, 1, 1, 3, 3, -10, 15, 1, 22, 23, 15, 7, 8, 0 };
 		Integer[] expectedNums1 = { 0, 1, 2, 3, -10, 15, 22, 23, 7, 8 };
-		
-		Integer[] nums2  = { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1};
+
+		Integer[] nums2 = { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 };
 		Integer[] expectedNums2 = { 1 };
-		
+
+		Integer[] nums3 = { 1, 2, 3, 4, 5, 6 };
+		Integer[] expectedNums3 = { 1, 2, 3, 4, 5, 6 };
+
 		assertArrayEquals(expectedNums1, MyArrays.removeRepeated(nums1));
 		assertArrayEquals(expectedNums2, MyArrays.removeRepeated(nums2));
-		
+		assertArrayEquals(expectedNums3, MyArrays.removeRepeated(nums3));
 	}
-	
+
 	@Test
 	void containsTest() {
 		assertTrue(MyArrays.contains(numbers, 47));
 		assertFalse(MyArrays.contains(numbers, 48));
-		
+
 		assertTrue(MyArrays.contains(strings, "ab"));
 		assertFalse(MyArrays.contains(strings, "abc"));
 	}
